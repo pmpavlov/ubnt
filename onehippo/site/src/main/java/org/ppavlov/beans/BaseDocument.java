226c73eef82077ab15da97b91353b1fbbd8dce17
Pavel Pavlov <ppavlov@outlook.com>
2016-03-25 19:07:57 +0000

    Adding the project

---
+++ onehippo/site/src/main/java/org/ppavlov/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.ppavlov.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="onehippo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
