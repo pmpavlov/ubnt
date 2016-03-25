1caa701b13bab0e39ad403484ad500cbb24ef84a
Pavel Pavlov <ppavlov@outlook.com>
2016-03-25 19:26:50 +0000

    Remove one hippo to heavy or Jenkins

--- onehippo/site/src/main/java/org/ppavlov/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.ppavlov.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="onehippo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
